aecbe0d356d7c83bffac0f0aadfebcb64a7e116b
faresben096-ops <faresben096@gmail.com>
2026-05-24 10:06:27 +0000

    Update bot-authme-first.js

--- bot-authme-first.js
+++ bot-authme-first.js
@@ -4,9 +4,9 @@ const { pathfinder, Movements, goals } = require('mineflayer-pathfinder');
 // Configuration - Edit these values for your server
 const config = {
   server: {
-    host: 'localhost', // Change to your server IP
-    port: 25565,
-    version: '1.20.4' // Change to your server version
+    host: 'dynamic-8.magmanode.com', // Change to your server IP
+    port: 25631,
+    version: '1.21.11' // Change to your server version
   },
   bot: {
     username: 'AFKBot', // Change to your desired bot name
